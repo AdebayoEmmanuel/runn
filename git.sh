d4c616796d82052a5b88032f28c92d6744e0101a
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2025-01-22 19:39:08 +0100

    Remove obsolete files and scripts, including .gitignore, README.md, configuration examples, and utility modules

--- git.sh
+++
@@ -1,7 +0,0 @@
-echo "# pyreq" >> README.md
-git init
-git add .
-git commit -m "Boilerplate update"
-git branch -M main
-git remote add git@github.com:AdebayoEmmanuel/pyreq.git
-git push -u origin main
